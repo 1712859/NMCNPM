2baa1eba3119e841899c1451d043d4cbbb5d7de9
Tú tú <tubato1999@gmail.com>
2020-01-05 23:32:15 +0700

    uppdate...

--- GroupManagement/app/src/main/java/com/example/groupmanagement/ui/home/AdapterRoom.java
+++ GroupManagement/app/src/main/java/com/example/groupmanagement/ui/home/AdapterRoom.java
@@ -4,70 +4,54 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.groupmanagement.R;
 
 import java.util.List;
 
-public class AdapterRoom extends RecyclerView.Adapter<AdapterRoom.MyViewHolder> {
+public class AdapterRoom extends ArrayAdapter<Room> {
 
     private List<Room> roomList;
-    private LayoutInflater mLayoutInflater;
+    private int mResource;
     private Context mContext;
 
-    public AdapterRoom(Context context, List<Room> roomList) {
-        this.roomList = roomList;
+    public AdapterRoom( Context context, int resource,  List<Room> objects) {
+        super(context, resource, objects);
+        this.roomList = objects;
         this.mContext = context;
-        mLayoutInflater = LayoutInflater.from(context);
+        this.mResource = resource;
     }
 
-    public class MyViewHolder extends RecyclerView.ViewHolder {
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        ViewHolder viewHolder = null;
+        if(viewHolder==null) {
 
-        public TextView name;
-
-        public MyViewHolder(View itemView) {
-            super(itemView);
-            name = (TextView) itemView.findViewById(R.id.recycler_room).findViewById(R.id.name_room);
-
-
-            itemView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Toast.makeText(mContext, name.getText(), Toast.LENGTH_SHORT).show();
-                }
-            });
-            itemView.setOnLongClickListener(new View.OnLongClickListener() {
-                @Override
-                public boolean onLongClick(View v) {
-                    Toast.makeText(mContext, "Long item clicked " + name.getText(), Toast.LENGTH_SHORT).show();
-                    return true;
-                }
-            });
+            convertView = LayoutInflater.from(mContext).inflate(mResource, parent, false);
+            viewHolder.NameRoom = convertView.findViewById(R.id.name_room);
         }
-    }
+        else
+            viewHolder = (ViewHolder)convertView.getTag();
+        final Room rom = roomList.get(position);
 
-
-    @Override
-    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View item = mLayoutInflater.inflate(R.layout.fragment_home, parent, false);
-        return new MyViewHolder(item);
-    }
-
-    @Override
-    public void onBindViewHolder(MyViewHolder holder, int position) {
-        Room room1 = roomList.get(position);
-        holder.name.setText(room1.getName());
+        return  convertView;
 
     }
 
-    @Override
-    public int getItemCount() {
-        return roomList.size();
+    public class ViewHolder {
+        TextView NameRoom;
+
+
     }
-
-
 }
+
+
+
--- GroupManagement/app/src/main/java/com/example/groupmanagement/ui/home/HomeFragment.java
+++ GroupManagement/app/src/main/java/com/example/groupmanagement/ui/home/HomeFragment.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.ListView;
 
 
 import androidx.annotation.NonNull;
@@ -18,40 +18,40 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.groupmanagement.R;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
+
 public class HomeFragment extends Fragment {
 
-    private HomeViewModel homeViewModel;
-    private List<Room> mListRoom = new ArrayList<>();
-    private RecyclerView recyclerView;
-    private AdapterRoom adapterRoom;
+    RecyclerView recyclerView;
+    List roomlist = new ArrayList<>() ;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
+    public View onCreateView( LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-//        homeViewModel = ViewModelProviders.of(this).get(HomeViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_home, container, false);
-        // get data tư api
-        prepareRoomData();
-        recyclerView = root.findViewById(R.id.recycler_room);
-        adapterRoom = new AdapterRoom(getActivity(),mListRoom);
+        recyclerView.findViewById(R.id.list);
+        onsetdata();
 
-        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(container.getContext());
-        recyclerView.setLayoutManager(mLayoutManager);
-        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        return inflater.inflate(R.layout.fragment_home,container,false);
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        AdapterRoom adapterRoom = new AdapterRoom(getContext(),R.layout.fragment_home,roomlist);
         recyclerView.setAdapter(adapterRoom);
-        return root;
-
     }
 
-    private void prepareRoomData() {
-        Room movie = new Room("aaaaaa");
-        mListRoom.add(movie);
-        Room movie1 = new Room("bbbbb");
-        mListRoom.add(movie);
-        Room movie2 = new Room("vvvvv");
-        mListRoom.add(movie);
+    private void onsetdata()
+    {
+        roomlist.add(new Room("nhóm 1"));
+        roomlist.add(new Room("nhóm 2"));
+        roomlist.add(new Room("nhóm 3"));
+        roomlist.add(new Room("nhóm 4"));
     }
+
+
 
 }
