bb83a8f4baf28a7dd718f740df4c9622758c355c
Tú tú <tubato1999@gmail.com>
2020-01-07 17:15:57 +0700

    update room

---
+++ GroupManagement/app/src/main/java/com/example/groupmanagement/addMission.java
@@ -0,0 +1,15 @@
+package com.example.groupmanagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class addMission extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_mission);
+        setTitle("Thêm nhiệm vụ");
+    }
+}
---
+++ GroupManagement/app/src/main/java/com/example/groupmanagement/ui/home/addMember.java
@@ -0,0 +1,18 @@
+package com.example.groupmanagement.ui.home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.groupmanagement.R;
+
+public class addMember extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_member);
+
+        setTitle("Thêm thành viên");
+    }
+}
